2f0410e60c8d079dd08c849982f47b5047d77ae9
Tomo4kap <tomo4kap@gmail.ru>
2023-04-10 19:13:28 +0300

    Homework even/odd numbers

---
+++ LESSON 1 HOMEWORK 3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter an integer: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = number1 % 2;
+if (number2 == 0)
+{
+    Console.WriteLine($"{number1} is an even number");
+}
+else
+{
+    Console.WriteLine($"{number1} is an odd number");
+}
